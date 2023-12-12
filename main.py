0ddebe3137d1970e89a912fcc2ce6e20fa8c5b87
dandotcraig <dandotcraig@gmail.com>
2023-12-13 08:48:44 +1100

    added choices - ending session for this morning

--- main.py
+++ main.py
@@ -5,4 +5,21 @@ def home_menu():
     print("2. Enter 2 to update exercises in your list")
     print("3. Enter 3 to remove exercises in your list")
     print("4. Enter 4 to view workout to your list")
-    print("5. Enter 5 to add exercises to your list")
+    print("5. Enter 5 to view your history")
+    choice = input("Enter your selection: ")
+    return choice
+
+user_choice = ""
+
+while user_choice != "5":
+    user_choice = home_menu()
+    if (user_choice == "1"):
+        print("You entered 1")
+    elif (user_choice == "2"):
+        print("You entered 2")
+    elif (user_choice == "3"):
+        print("You entered 3")
+    elif (user_choice == "4"):
+        print("You entered 4")
+    elif (user_choice == "5"):
+        print("You entered 45")
