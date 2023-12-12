b03029b72b1d7a3c7f77881fe2c8c7795d8789a2
dandotcraig <dandotcraig@gmail.com>
2023-12-13 08:36:29 +1100

    added menu

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 print("welcome to your gym tracker")
+
+def home_menu():
+    print("1. Enter 1 to add exercises to your list")
+    print("2. Enter 2 to update exercises in your list")
+    print("3. Enter 3 to remove exercises in your list")
+    print("4. Enter 4 to view workout to your list")
+    print("5. Enter 5 to add exercises to your list")
