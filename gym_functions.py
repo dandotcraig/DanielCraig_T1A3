f373e15d5a74b88430509a4820f4e0ef82223e7a
dandotcraig <dandotcraig@gmail.com>
2023-12-14 16:25:17 +1100

    added more menu items

--- gym_functions.py
+++ gym_functions.py
@@ -1,11 +1,20 @@
 def add_exercise():
-    print(add_exercise)
+    print("add exercise")
+
+def update_exercise():
+    print("update exercise")   
 
 def remove_exercise():
-    print(remove_exercise)
+    print("remove exercise")
 
 def add_weight():
-    print(add_weight)
+    print("addweight")
 
 def update_weight():
-    print(update_weight)
+    print("update weight")
+
+def remove_weight():
+    print("remove weight")
+
+def view_list():
+    print("view list")
