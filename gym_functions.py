bf0f8f9601059bff508a4397c40306b7c636046e
dandotcraig <dandotcraig@gmail.com>
2023-12-14 14:46:36 +1100

    added first functions

---
+++ gym_functions.py
@@ -0,0 +1,11 @@
+def add_exercise():
+    print(add_exercise)
+
+def remove_exercise():
+    print(remove_exercise)
+
+def add_weight():
+    print(add_weight)
+
+def update_weight():
+    print(update_weight)
